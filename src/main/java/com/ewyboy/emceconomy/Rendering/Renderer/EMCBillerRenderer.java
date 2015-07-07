f1f38fb1116276c14652e89e7c0d5f737902a66b
Ewy <EwyBoy@gmail.com>
2015-07-07 02:05:18 +0200

    Rendering and textures

--- src/main/java/com/ewyboy/emceconomy/Rendering/Renderer/EMCBillerRenderer.java
+++ src/main/java/com/ewyboy/emceconomy/Rendering/Renderer/EMCBillerRenderer.java
@@ -1,7 +1,9 @@
 package com.ewyboy.emceconomy.Rendering.Renderer;
 
+import com.ewyboy.emceconomy.Core.EMCEconomy;
 import com.ewyboy.emceconomy.Rendering.Models.EMCBillerModel;
-import com.pahimar.ee3.util.ResourceLocationHelper;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.OpenGlHelper;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.entity.Entity;
 import net.minecraft.tileentity.TileEntity;
@@ -11,8 +13,6 @@ import org.lwjgl.opengl.GL11;
 public class EMCBillerRenderer extends TileEntitySpecialRenderer {
 
     private final EMCBillerModel model;
-    private static final ResourceLocation EMCBillerTexture = ResourceLocationHelper.getResourceLocation("textures/models/EMCBiller.png");
-
     public EMCBillerRenderer() {
         this.model = new EMCBillerModel();
     }
@@ -21,8 +21,9 @@ public class EMCBillerRenderer extends TileEntitySpecialRenderer {
     public void renderTileEntityAt(TileEntity entity, double x, double y, double z, float scale) {
         GL11.glPushMatrix();
             GL11.glTranslatef((float) x + 0.5F, (float) y + 1.5F, (float) z + 0.5F);
-
-            this.bindTexture(EMCBillerTexture);
+            Minecraft.getMinecraft().renderEngine.bindTexture(new ResourceLocation(EMCEconomy.ModID + ":" + "textures/models/EMCBiller.png"));
+            OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, 242, 242);
+            GL11.glColor3f(0.0F,1.0F,0.0F);
                 GL11.glPushMatrix();
                     GL11.glRotatef(180F, 0.0F, 0.0F, 1.0F);
                     this.model.render((Entity)null, 0.0F, 0.0F, -0.1F, 0.0F, 0.0F, 0.0625F);
